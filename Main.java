c75b2059fc18472a1cd7718c735ec93762a54fd5
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-05-12 11:42:50 +0500

    m

--- Main.java
+++ Main.java
@@ -2,5 +2,15 @@
 public class Main{
     public static void main(String[]args){
         System.out.println("Hello world");
-    }
+        System.out.println("My name is Nadeem Ashraf");
+        int a = 25;
+        int b = 38;
+        int sum=a+b;
+        System.out.println(sum);
+        String name="nadeem ashraf";
+        String father = "ashraf hussain";
+        String concaString=name+father;
+        System.out.println(concaString);
+
+     }
 }
---
+++ dataType.java
@@ -0,0 +1,11 @@
+//Data type in java 
+//we have multiple datatype in java 
+//string datatype
+public class dataType {
+    public static void main(String[] args) {
+        String name = "Nadeem";
+        int rollNumber = 4;
+        System.out.println(name);
+        System.out.println(rollNumber);
+    }
+}
