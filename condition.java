832d4c888d1a494dd76389a9af52b395c6ab31cb
Nadeem-ashraf004 <nadeemmsaqi@gmail.com>
2025-05-16 09:32:00 +0500

    m

---
+++ condition.java
@@ -0,0 +1,13 @@
+public class condition {
+    public static void main(String[]arge){
+        int x=23;
+        int y=44;
+        if (x>=y) {
+            System.out.println("your a fantastic guye !");
+            }
+        else{
+            System.out.println("you are right !");
+        }    
+        
+    }
+}
